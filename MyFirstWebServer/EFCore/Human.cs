7c05760d8d467c4cc9e6701ac9fba809231346c0
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2025-03-28 22:01:19 +0100

    addedAppWithEFAndSQLite

---
+++ MyFirstWebServer/EFCore/Human.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleAppWithEFCore
+{
+    public class Human
+    {
+        private DateTime _Birthdate;
+        public DateTime Birthdate { get { return _Birthdate; } private set { _Birthdate = value; } }
+        public int Age { get { return DateTime.Now.Year - _Birthdate.Year; } }
+        public enum AllowedGenders
+        {
+            m = 0,
+            w = 1,
+            d = 2
+        }
+        private AllowedGenders _Gender;
+        public AllowedGenders Gender 
+        { 
+            get { return _Gender; } 
+            set { _Gender = value; } 
+        }
+        public Human() {}
+        public Human(DateTime birthdate, AllowedGenders gender) 
+        {
+            _Birthdate = birthdate;
+            _Gender = gender;
+        }
+        public void UpdateBirthdate(DateTime newBirthdate)
+        {
+            Birthdate = newBirthdate;
+        }
+    }
+}
---
+++ MyFirstWebServer/EFCore/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleAppWithEFCore
+{
+    public class Person : Human
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Person(string firstName, string lastName, DateTime birthdate, AllowedGenders gender) : base(birthdate, gender)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
